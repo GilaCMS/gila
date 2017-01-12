d6264bde5c5d159977a3001899474a880533d6e7
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2017-01-12 10:49:46 -0600

    Update gila.php

--- src/core/classes/gila.php
+++ src/core/classes/gila.php
@@ -3,6 +3,23 @@
 //  common functions will be put here
 
 class gila {
+  
+  
+  static function controllers($key, $list)
+  {
+    if (!is_array($list)) $list[0]=$list;
+    foreach ($list as $k=>$item) {
+      $GLOBALS[$key][$k]=$item;
+    }
+  }
+  
+  static function widgets($key, $list)
+  {
+    if (!is_array($list)) $list[0]=$list;
+    foreach ($list as $k=>$item) {
+      $GLOBALS[$key][$k]=$item;
+    }
+  }
 
   static function post($id)
   {
