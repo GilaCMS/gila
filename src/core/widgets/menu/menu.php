fbddfc0f00d055643b17e9d5ed68c1ce5a202b7f
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2017-05-12 23:44:50 -0500

    bugfixes

--- src/core/widgets/menu/menu.php
+++ src/core/widgets/menu/menu.php
@@ -2,20 +2,27 @@
 <?php
 $menu_items = json_decode($widget_data,true);
 
-foreach ($menu_items as $mi) {
-        if (!isset($mi['children'])) {
-            echo "<li><a href=\"{$mi['url']}\">{$mi['title']}</a></li>";
-        }
-        else {
-            echo "<li>";
-            echo "<a href=\"{$mi['url']}\" >{$mi['title']}</a>";
-            if (isset($mi['children'][0])) {
-                echo "<ul class=\"dropdown-menu\" role=\"menu\">";
-                foreach ($mi['children'][0] as $mii) if(isset($mii['url'])){
-                    echo "<li><a href=\"{$mii['url']}\">{$mii['title']}</a></li>";
-                }
-                echo "</ul></li>";
+echo_menu_items($menu_items);
+
+function echo_menu_items($menu_items)
+{
+    foreach ($menu_items as $mi) {
+            if (!isset($mi['children'])) {
+                echo "<li><a href=\"{$mi['url']}\">{$mi['title']}</a></li>";
             }
-        }
-} ?>
+            else {
+                echo "<li>";
+                echo "<a href=\"{$mi['url']}\" >{$mi['title']}</a>";
+                if (isset($mi['children'][0])) if (isset($mi['children'][0][0])) {
+                    echo "<ul class=\"dropdown-menu\" role=\"menu\">";
+                    foreach ($mi['children'][0] as $mii) if(isset($mii['url'])){
+                        echo "<li><a href=\"{$mii['url']}\">{$mii['title']}</a></li>";
+                    }
+                    echo "</ul></li>";
+                }
+            }
+    }
+}
+
+ ?>
 </ul>
