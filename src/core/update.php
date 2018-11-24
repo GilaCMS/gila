badcf9ba96c2c029bce1be7b3c3125fb9d5a6ced
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2018-11-24 10:10:07 -0600

    add tests folder

--- src/core/update.php
+++ src/core/update.php
@@ -1,7 +1,7 @@
 <?php
 
 if(version_compare($GLOBALS['version'],'1.7.5') < 0) {
-    file_put_contents("lib/gila.min1.css",file_get_contents("src/core/lib/gila.min.css"));
+    file_put_contents("lib/gila.min.css",file_get_contents("src/core/lib/gila.min.css"));
 }
 
 if(version_compare($GLOBALS['version'],'1.7.6') < 0) {
