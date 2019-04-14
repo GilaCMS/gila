b2b6d860ce39258d9f07ea6c00b4ecf719fcd3d2
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2019-04-13 19:12:44 -0500

    contact-form

--- src/core/widgets/contact-form/contact-form.php
+++ src/core/widgets/contact-form/contact-form.php
@@ -1,23 +1,25 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && event::get('recaptcha',true)
+    && $_POST['wdgtid']==$widget_data->widget_id) {
+  new sendmail(["post"=>["name","email","subject"]]);
+  view::alert('success', $widget_data->success_msg);
+}
+?>
+
 <form role="form" method="post" action="<?=$_SERVER['REQUEST_URI']?>" class="g-form wrapper g-card">
   <input type="hidden" name="wdgtid" value="<?=$widget_data->widget_id?>">
-  <?php
-  if ($_SERVER['REQUEST_METHOD'] === 'POST' && event::get('recaptcha',true) && $_POST['wdgtid']==$widget_data->widget_id) {
-    new sendmail(["post"=>["name","email","subject"]]);
-    view::alert('success', $widget_data->success_msg);
-    view::alerts();
-  }
-  ?>
-  <label><?=_("Name")?></label>
+  <?php view::alerts() ?>
+  <label><?=__("Name")?></label>
   <div class="form-group">
     <input class="form-control fullwidth" name="name" autofocus required>
   </div>
-  <label><?=_("E-mail")?></label>
+  <label><?=__("E-mail")?></label>
   <div class="form-group">
     <input class="form-control fullwidth" name="email" type="email" required>
   </div>
-  <label><?=_("Subject")?></label>
+  <label><?=__("Subject")?></label>
   <div class="form-group ">
-    <input class="form-control fullwidth" name="subject" required>
+    <textarea class="form-control fullwidth" name="subject" required></textarea>
   </div>
   <?php event::fire('recapcha.form')?>
   <input type="submit" class="btn btn-primary btn-block">
