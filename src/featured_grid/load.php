520ecb890a7162432a835cb1d237a39035645495
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2019-04-19 22:20:24 -0500

    widget fixes

--- src/featured_grid/load.php
+++ src/featured_grid/load.php
@@ -1,6 +1,6 @@
 <?php
 
-if(gila::option('featured_grid.category')!='400px') {
+if(gila::option('featured_grid.height')=='400px') {
     view::stylesheet('src/featured_grid/assets/style400.css');
 } else view::stylesheet('src/featured_grid/assets/style.css');
 
@@ -12,9 +12,10 @@ event::listen('slide',function(){
         if(gila::option('featured_grid.category')!='') $params['category']=gila::option('featured_grid.category');
         foreach (blog::posts($params) as $p) {
             $srcset = view::thumb_srcset($p['img'],[600,240]);
-            echo "<div class=\"img\" style=\"background-image: url('{$srcset[0]}');";
-            echo "background-image: -webkit-image-set(url({$srcset[0]}) 1x, url({$srcset[1]}) 2x);\">";
+            echo "<div>";
             echo "<a href=\"".blog::get_url($p['id'],$p['slug'])."\">";
+            echo "<div class=\"img\" style=\"background-image: url('{$srcset[0]}');";
+            echo "background-image: -webkit-image-set(url({$srcset[0]}) 1x, url({$srcset[1]}) 2x);\"></div>";
             echo "<div class=\"featured-title\">{$p['title']}</div>";
             echo "</a></div>";
         }
