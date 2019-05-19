b5d02081ce3e3342b7b3e0fe4f1566066ba26705
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2019-05-19 18:00:46 -0500

    v1.10.5

--- src/core/views/admin/media-assets.php
+++ src/core/views/admin/media-assets.php
@@ -6,26 +6,37 @@ $disabled = ($path=='')?'disabled':'';
 
 $files=[];
 if($path=='src') {
-  $scanned = scandir('src/core/assets/');
-  foreach($scanned as $i=>$v){ $scanned[$i] = 'src/core/assets/'.$scanned[$i];}
-  $files = array_merge($files, $scanned);
+  $scanned = scandir('src/');
+  foreach($scanned as $i=>$v) if(is_dir('src/'.$v)) {
+    $package = json_decode(file_get_contents('src/'.$v.'/package.json'));
+    if(isset($package->assets)) {
+      foreach($package->assets as $asset) {
+        $files[] = 'src/'.$v.'/'.$asset;
+      }
+    }
+  }
+/*  $files = array_merge($files, $scanned);
   foreach(gila::packages() as $p) {
     $scanned = scandir('src/'.$p.'/assets/');
     foreach($scanned as $i=>$v) {    
       $scanned[$i] = 'src/'.$p.'/assets/'.$scanned[$i];
     }
     if($scanned) $files = array_merge($files, $scanned);
-  }
+  }*/
 } else {
   $path_array = explode('/',$path);
   array_splice($path_array,count($path_array)-1);
-  $uppath=implode('/',$path_array);
+  if(count($path_array)<3) {
+    $uppath = 'src';
+  } else {
+    $uppath=implode('/',$path_array);
+  }
   $path = rtrim($path, '/');
   $files = scandir($path);
   foreach($files as $i=>$v) {
-    $files[$i] = $path.$files[$i];
+    $files[$i] = $path.'/'.$files[$i];
   } ?>
-<a class='btn btn-white g-group-item' id='fm-goup' data-path='src' <?=$disabled?>>
+<a class='btn btn-white g-group-item' id='fm-goup' data-path='<?=$uppath?>' <?=$disabled?>>
 <i class='fa fa-arrow-left'></i></a>
 <span class='g-group-item' style="padding:var(--main-padding)"><?=$path?></span>
 
@@ -50,6 +61,10 @@ foreach($files as $filepath) if(substr($filepath, -1)!='.') {
   }
 
   $basename = substr($filepath, strrpos($filepath, '/', -1)+1);
+  if($path=='src') {
+    $folders = explode('/', $filepath);
+    $basename = $folders[1].':'.$basename;
+  }
 
   if ($type=='image') {
     $img='<img src="'.view::thumb($filepath,'media_thumb/',100).'">';
