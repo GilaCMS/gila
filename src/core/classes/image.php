ac0035fa2a73b2995b9ecfa28a9dfc8b8cdb86f8
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2019-06-01 20:00:11 -0500

    user without password

--- src/core/classes/image.php
+++ src/core/classes/image.php
@@ -168,7 +168,7 @@ class image
   static function local_path($src)
   {
     if(parse_url($src, PHP_URL_HOST) != null) if(strpos($src,gila::config('base')) !== 0) {
-      $_src = 'tmp/'.str_replace(["://",":\\\\","\\","/",":"], "_", $src);
+      $_src = SITE_PATH.'tmp/'.str_replace(["://",":\\\\","\\","/",":"], "_", $src);
       if(!file_exists($_src)) {
         $_file = 'log/cannot_copy.json';
         $cannot_copy = json_decode(file_get_contents($_file),true);
