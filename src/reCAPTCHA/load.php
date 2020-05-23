5d24e34c07091ac90b46b7ae46137ceeb149d6da
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2020-05-23 12:16:39 -0500

    revert recaptcha

---
+++ src/reCAPTCHA/load.php
@@ -0,0 +1,21 @@
+<?php
+
+Event::listen('recaptcha.form',function(){
+	$sitekey = Gila::option('reCAPTCHA.site_key');
+    if($sitekey=='') return;
+    ?>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <div class="g-recaptcha" data-sitekey="<?=$sitekey?>"></div>
+	<?php
+});
+
+Event::listen('recaptcha',function(){
+  $secret = Gila::option('reCAPTCHA.secret_key');
+  if($secret=='') return false;
+  if(!isset($_POST['g-recaptcha-response'])) return false;
+
+  $response = new gpost('https://www.google.com/recaptcha/api/siteverify',
+    ['secret' => $secret, 'response' => $_POST['g-recaptcha-response']],
+    ['type'=>'x-www-form-urlencoded']);
+  return json_decode($response)->success;
+});
