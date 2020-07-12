7fd5248454233ae62020cea9c651588d0f14997c
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2020-07-12 12:28:29 -0500

    fix test

--- tests/phpunit/class-View.php
+++ tests/phpunit/class-View.php
@@ -3,13 +3,16 @@
 include(__DIR__.'/includes.php');
 include(__DIR__.'/../../src/core/classes/Image.php');
 use PHPUnit\Framework\TestCase;
+use Gila\FileManager;
+use Gila\View;
+use Gila\Gila;
 
 class ClassView extends TestCase
 {
 	public function test_getWidgetBody()
 	{
-		Gila\Gila::widgets(['paragraph'=>'core/widgets/paragraph']);
-		$html = Gila\Gila\View::getWidgetBody('paragraph', ['text'=>'Hello world']);
+		Gila::widgets(['paragraph'=>'core/widgets/paragraph']);
+		$html = View::getWidgetBody('paragraph', ['text'=>'Hello world']);
 		$this->assertEquals('<p>Hello world</p>', $html);
 	}
 
@@ -20,13 +23,13 @@ class ClassView extends TestCase
 		file_put_contents($jpg1, '');
 		file_put_contents($jpg2, '');
 		file_put_contents('data/uploads/.thumbs.json', '{"1jpg200":"firstpath"}');
-		$file = Gila\View::getThumbName('data/uploads/1.jpg', 200);
+		$file = View::getThumbName('data/uploads/1.jpg', 200);
 		$this->assertEquals("firstpath", $file);
-		$file200 = Gila\View::getThumbName($jpg2, 200);
-		$file300 = Gila\View::getThumbName($jpg2, 300);
+		$file200 = View::getThumbName($jpg2, 200);
+		$file300 = View::getThumbName($jpg2, 300);
 		$this->assertFalse($file200 == $file300);
-		$newfile200 = Gila\View::getThumbName($jpg2, 200);
-		$newfile300 = Gila\View::getThumbName($jpg2, 300);
+		$newfile200 = View::getThumbName($jpg2, 200);
+		$newfile300 = View::getThumbName($jpg2, 300);
 		$this->assertEquals($file200, $file200);
 		$this->assertEquals($file300, $file300);
 		FileManager::delete($jpg1);
