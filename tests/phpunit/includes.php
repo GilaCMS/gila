6cf690375f25065ce79bcaa2db38e95f3c8f4c59
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2020-07-12 15:24:01 -0500

    gila namespace

--- tests/phpunit/includes.php
+++ tests/phpunit/includes.php
@@ -1,14 +1,14 @@
 <?php
 
 chdir(__DIR__.'/../../');
-include_once(__DIR__.'/../../vendor/autoload.php');
-include_once(__DIR__.'/../../src/core/classes/Gila.php');
-include_once(__DIR__.'/../../src/core/classes/Router.php');
-include_once(__DIR__.'/../../src/core/classes/FileManager.php');
-include_once(__DIR__.'/../../src/core/classes/Db.php');
-//include_once(__DIR__.'/../../src/core/classes/Session.php');
-include_once(__DIR__.'/../../src/core/classes/View.php');
-include_once(__DIR__.'/../../src/core/classes/Event.php');
+include_once __DIR__.'/../../vendor/autoload.php';
+include_once __DIR__.'/../../src/core/classes/Gila.php';
+include_once __DIR__.'/../../src/core/classes/Router.php';
+include_once __DIR__.'/../../src/core/classes/FileManager.php';
+include_once __DIR__.'/../../src/core/classes/Db.php';
+include_once __DIR__.'/../../src/core/classes/Session.php';
+include_once __DIR__.'/../../src/core/classes/View.php';
+include_once __DIR__.'/../../src/core/classes/Event.php';
 define('SITE_PATH', '');
 define('LOG_PATH', 'log');
 define('CONFIG_PHP', 'config.php');
--- tests/phpunit/requests.php
+++ tests/phpunit/requests.php
@@ -1,14 +1,14 @@
 <?php
 
-include(__DIR__.'/includes.php');
-//include(__DIR__.'/../../src/core/classes/Controller.php');
-//include(__DIR__.'/../../src/core/classes/Package.php');
-//include(__DIR__.'/../../src/core/models/User.php');
-//include(__DIR__.'/../../src/core/classes/Table.php');
-//include(__DIR__.'/../../src/core/classes/TableSchema.php');
-//include(__DIR__.'/../../src/core/classes/Form.php');
-//include(__DIR__.'/../../src/core/classes/Sendmail.php');
-//include(__DIR__.'/../../src/core/classes/HtmlInput.php');
+include __DIR__.'/includes.php';
+include __DIR__.'/../../src/core/classes/Controller.php';
+include __DIR__.'/../../src/core/classes/Package.php';
+include __DIR__.'/../../src/core/models/User.php';
+include __DIR__.'/../../src/core/classes/Table.php';
+include __DIR__.'/../../src/core/classes/TableSchema.php';
+include __DIR__.'/../../src/core/classes/Form.php';
+include __DIR__.'/../../src/core/classes/Sendmail.php';
+include __DIR__.'/../../src/core/classes/HtmlInput.php';
 use PHPUnit\Framework\TestCase;
 use Gila\Gila;
 use Gila\Event;
