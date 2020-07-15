07c6e82d067c02363569c79fb7efb3d39a5b38b2
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2020-07-15 07:38:19 -0500

    namespaces

--- src/core/views/login-header.php
+++ src/core/views/login-header.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="<?=Config::config('language')?>">
+<html lang="<?=Gila\Config::config('language')?>">
 
 <head>
-  <base href="<?=Config::base_url()?>">
+  <base href="<?=Gila\Config::base_url()?>">
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title><?=Config::config('title')?> - <?=($title??'')?></title>
+  <title><?=Gila\Config::config('title')?> - <?=($title??'')?></title>
 
-  <?=View::css('core/gila.min.css')?>
-  <?=View::css('lib/font-awesome/css/font-awesome.min.css')?>
+  <?=Gila\View::css('core/gila.min.css')?>
+  <?=Gila\View::css('lib/font-awesome/css/font-awesome.min.css')?>
 </head>
 
 <body style="background: var(--main-bg-color)">
