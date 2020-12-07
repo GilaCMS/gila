e0282c17caa9388f3de92a28334ec513286078ff
Jocelyn Reyes <yoce_ab@hotmail.com>
2020-12-07 17:24:37 -0600

    se añade clase userTest

--- tests/phpunit/UserTest.php
+++ tests/phpunit/UserTest.php
@@ -5,11 +5,17 @@ include_once DIR.'/../../src/core/classes/User.php';
 use PHPUnit\Framework\TestCase;
 use Gila\User;
 
-
-
-public static function test_create(){
+/**
+ * 
+ */
+class userTest
+{
+	public static function test_create(){
 		$result = User::create('test_create@email.com', '123');
 		$this->assertTrue($result);
 		$result = User::create('test_create@email.com', '123');
 		$this->assertTrue($result);
 	}
+}
+
+
