4e6125289bd263d5f005caa3efd8bec3ddbb45e1
Jocelyn Reyes <yoce_ab@hotmail.com>
2020-12-07 14:26:20 -0600

    archivo UserTest.php

---
+++ tests/phpunit/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+include DIR.'/../../vendor/autoload.php';
+include_once DIR.'/../../src/core/classes/User.php';
+use PHPUnit\Framework\TestCase;
+use Gila\User;
+
+
+
+public static function test_create(){
+		$result = User::create('test_create@email.com', '123');
+		$this->assertTrue($result);
+		$result = User::create('test_create@email.com', '123');
+		$this->assertTrue($result);
+	}
