25c204378c1912bbe50a924b2d16c1c52f530d12
Jocelyn Reyes <yoce_ab@hotmail.com>
2020-12-08 17:47:33 -0600

    static delete

--- tests/phpunit/UserTest.php
+++ tests/phpunit/UserTest.php
@@ -10,7 +10,7 @@ use Gila\User;
  */
 class UserTest extends TestCase
 {
-	public static function test_create(){
+	public function test_create(){
 		$result = User::create('test_create@email.com', '123');
 		$this->assertTrue($result);
 		$result = User::create('test_create@email.com', '123');
