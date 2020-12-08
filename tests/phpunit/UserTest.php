2fb0312e928839d25b789e3e2d3580b747582d43
Jocelyn Reyes <yoce_ab@hotmail.com>
2020-12-07 19:15:55 -0600

    corrección de DIR y Extends

--- tests/phpunit/UserTest.php
+++ tests/phpunit/UserTest.php
@@ -1,14 +1,14 @@
 <?php
 
-include DIR.'/../../vendor/autoload.php';
-include_once DIR.'/../../src/core/classes/User.php';
+include _DIR.'/../../vendor/autoload.php';
+include_once _DIR.'/../../src/core/classes/User.php';
 use PHPUnit\Framework\TestCase;
 use Gila\User;
 
 /**
  * 
  */
-class userTest
+class userTest extends TestCase
 {
 	public static function test_create(){
 		$result = User::create('test_create@email.com', '123');
