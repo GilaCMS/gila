7ab5db9c9ef1a0a3231b68bb21a2318e05c1bb61
Fernando Piedra <fernandopiedram@gmail.com>
2022-03-24 16:39:52 -0600

    implement bootstrap

--- src/core/widgets/features/features.php
+++ src/core/widgets/features/features.php
@@ -1,8 +1,9 @@
 <section>
-<div class="features-grid">
-<?=View::css('core/widgets.css')?>
+<div class="d-flex flex-wrap justify-content-center">
+<?=View::script('lib/bootstrap5/bootstrap.bundle.min.js')?>
+<?=View::cssAsync('lib/bootstrap5/bootstrap.min.css')?>
 <?php foreach (json_decode(@$widget_data->features) as $feature) { ?>
-    <div style="text-align:<?=htmlentities($data['align']??'center')?>">
+    <div class="m-3" style="text-align:<?=htmlentities($data['align']??'center')?>">
         <?=View::img($feature[0], 300)?>
         <h3><?=htmlentities($feature[1])?></h3>
         <p><?=htmlentities($feature[2])?></p>
