bb1431f75f875cf7781b20f128b9cc71080108f4
Fernando Piedra <fernandopiedram@gmail.com>
2022-03-25 16:16:21 -0600

    change

--- src/core/widgets/tag/tag.php
+++ src/core/widgets/tag/tag.php
@@ -1,4 +1,6 @@
 <?=View::css('core/gila.min.css')?>
+<?=View::script('lib/bootstrap5/bootstrap.bundle.min.js')?>
+<?=View::cssAsync('lib/bootstrap5/bootstrap.min.css')?>
 <div class="pad widget-tags">
 <?php
 $data['n'] ??= 12;
@@ -10,7 +12,7 @@ GROUP BY value ORDER BY n DESC LIMIT 0,{$data['n']};");
 
 while ($r=mysqli_fetch_array($res)) {
   $tag = trim($r[0]);
-  echo "<a class='g-btn btn-white' href='blog/tag/{$tag}'>{$tag}({$r[1]}) </a>";
+  echo "<a class='btn btn-light' href='blog/tag/{$tag}'>{$tag}({$r[1]}) </a>";
 }
 
 echo '</div>';
